78f3a4b454eadf507f0ae5e6983540b1f4e1e276
Enri2077 <enry2077@gmail.com>
2018-05-31 19:29:50 +0100

    added simple path following in follower

--- pedestrian_detector/include/follower/follower.h
+++ pedestrian_detector/include/follower/follower.h
@@ -1,22 +1,30 @@
 #ifndef PEDEST0RIAN_DETECTOR_FOLLOWER_H
 #define PEDESTRIAN_DETECTOR_FOLLOWER_H
 
-#include <ros/ros.h>
 #include <string>
 #include <vector>
-#include <std_msgs/String.h>
-#include "geometry_msgs/PoseStamped.h"
-#include "geometry_msgs/PoseWithCovarianceStamped.h"
-#include "geometry_msgs/PointStamped.h"
+
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 #include <tf_conversions/tf_eigen.h>
+
+// Messages
+#include <std_msgs/String.h>
+#include <std_msgs/UInt8MultiArray.h>
+#include <nav_msgs/Path.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PointStamped.h>
 #include <geometry_msgs/Twist.h>
-#include <move_base_msgs/MoveBaseAction.h>
+
+// Services
+#include <std_srvs/Empty.h>
+#include <nav_msgs/GetPlan.h>
+
+// Actions
 #include <actionlib/client/simple_action_client.h>
-#include "std_msgs/UInt8MultiArray.h"
-#include "nav_msgs/GetPlan.h"
-#include "std_srvs/Empty.h"
-#include <tf/transform_broadcaster.h>
+#include <move_base_msgs/MoveBaseAction.h>
 
 
 class Follower
@@ -24,48 +32,48 @@ class Follower
  public:
   Follower(); //constructor
   ~Follower(); //distructor
-
-  // get parameters from param server
-  void getParams();
-
-  // callback for event_in recieved msg;
-  void eventInCallBack(const geometry_msgs::PointStampedConstPtr& msg);
-
-  // callback for event_in recieved msg;
-  void robotPoseCallBack(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg);
   
-  // ros node main loop
-  void updateNavigationGoal();
-
-  void updateHeadPosition();
-
-  void clearCostmaps();
-  
-  // rotate head towards person
-  void rotateHead(geometry_msgs::Point p);
-
+  void setup();
   void loop();
 
-  // flag used to know when we have received a callback
-  bool new_person_position_received_;
+  void filteredPersonPositionCallback(const geometry_msgs::PointStampedConstPtr& msg);
+  void robotPoseCallBack(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg);
   
- private:  
-  // ros related variables
+  void updateNavigationGoal();
+  void updateHeadPosition();
+
+  void getParams();
+  void clearCostmaps();
+
+  
+ private:
   ros::NodeHandle nh_;
+
+  // Publishers
   ros::Publisher pub_pose_;
-  //ros::Publisher pub_event_in_;
   ros::Publisher pub_pose_in_;
   ros::Publisher pub_head_rot_;
+  ros::Publisher trajectory_publisher_;
+  ros::Publisher current_target_pose_publisher_;
+  ros::Publisher next_target_pose_publisher_;
+
+  // Subscribers
   ros::Subscriber filtered_person_position_subscriber_;
-  ros::Subscriber sub_robot_pose_;
+  ros::Subscriber robot_position_subscriber_;
+
+  // Service Clients
   ros::ServiceClient plan_client_;
   ros::ServiceClient clear_client_;
   
+  // Main following state
   bool following_enabled_; // TODO enable/disable with event topics
 
   // Type of navigation. Set by default through parameters. Can be updated dinamically to implement different navigation stategies.
   // TODO change dinamically (state machine?)
   std::string navigation_type_, navigation_stack_;
+
+  // flag used to know when we have received a callback
+  bool new_person_position_received_;
 
   // Position of the person from the Bayesian filter
   geometry_msgs::PointStamped filtered_person_position_;
@@ -73,15 +81,19 @@ class Follower
   // Position of the person in base_link.
   geometry_msgs::PointStamped relative_person_position_;
   
-  // // stores que msg that will be published on event_out topic
-  // geometry_msgs::PoseStamped event_out_msg_; // TODO remove unused
+  // Person's path, from the beginning untill the last received person's filtered position
+  std::vector<geometry_msgs::PointStamped> complete_person_path_;
+  nav_msgs::Path complete_person_trajectory_;
+
+  // Position in the person's path used as target for navigation
+  long unsigned int path_target_pointer_;
   
   // for storing the arguments that will be read from param server
   std::vector<std::string> script_arguments_;
 
   // out topics
   //std_msgs::String stop_;
-  //std_msgs::String start_;
+  //std_msgs::String current_robot_position_;
 
   // position of the head
   double head_pos; 
@@ -96,7 +108,7 @@ class Follower
 
   geometry_msgs::Point keptPoint_;
   
-  geometry_msgs::PoseStamped start_;
+  geometry_msgs::PoseStamped current_robot_position_;
 
   geometry_msgs::PoseStamped goal_;
   geometry_msgs::Point pointGoal_;
--- pedestrian_detector/src/follower/follower.cpp
+++ pedestrian_detector/src/follower/follower.cpp
@@ -2,18 +2,28 @@
 #include <string>
 #include <vector>
 
-Follower::Follower():new_person_position_received_(false), nh_(ros::NodeHandle()){
+Follower::Follower(): nh_(ros::NodeHandle()), new_person_position_received_(false){
 
-  nh_.param<std::string>("default_navigation_type", navigation_type_, "straight");
-  nh_.param<std::string>("default_navigation_stack", navigation_stack_, "move_base");
+  ros::NodeHandle private_nh = ros::NodeHandle("~");
+
+  if(!private_nh.param<std::string>("default_navigation_type", navigation_type_, "straight"))
+    ROS_WARN("Parameter default_navigation_type not set. Using default value.");
+
+  if(!private_nh.param<std::string>("default_navigation_stack", navigation_stack_, "move_base"))
+    ROS_WARN("Parameter default_navigation_stack not set. Using default value.");
 
   // subscriptions
-  filtered_person_position_subscriber_ = nh_.subscribe("person_position",1,&Follower::eventInCallBack,this);
-  sub_robot_pose_ = nh_.subscribe("/amcl_pose",1,&Follower::robotPoseCallBack,this);
+  filtered_person_position_subscriber_ = nh_.subscribe("person_position", 1, &Follower::filteredPersonPositionCallback, this);
+  robot_position_subscriber_ = nh_.subscribe("/amcl_pose", 1, &Follower::robotPoseCallBack, this);
 
   //pub_event_in_ = nh_.advertise<std_msgs::String>("/move_base_wrapper/event_in",2);
-  pub_pose_in_ = nh_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal",2);
-  pub_head_rot_ = nh_.advertise<std_msgs::UInt8MultiArray>("/cmd_head",2);
+  pub_pose_in_ = nh_.advertise<geometry_msgs::PoseStamped>("/move_base_simple/goal", 2);
+  pub_head_rot_ = nh_.advertise<std_msgs::UInt8MultiArray>("/cmd_head", 2);
+  
+  trajectory_publisher_ = nh_.advertise<nav_msgs::Path>("person_trajectory", 2);
+
+  current_target_pose_publisher_ = nh_.advertise<geometry_msgs::PoseStamped>("current_navigation_target", 2);
+  next_target_pose_publisher_ = nh_.advertise<geometry_msgs::PoseStamped>("next_navigation_target", 2);
   
   // querying parameters from parameter server TODO
   getParams();
@@ -22,12 +32,12 @@ Follower::Follower():new_person_position_received_(false), nh_(ros::NodeHandle()
   plan_client_ = nh_.serviceClient<nav_msgs::GetPlan>("/move_base/GlobalPlanner/make_plan");
   clear_client_ = nh_.serviceClient<std_srvs::Empty>("/move_base/clear_costmaps");
   
-  //start_.data = "e_start";
-  //stop_.data = "e_stop";
   listener_ = new (tf::TransformListener);
 
   // States
   following_enabled_ = true; // TODO
+
+  path_target_pointer_ = 0;
 
 }
 
@@ -40,22 +50,64 @@ Follower::~Follower(){
 void Follower::getParams(){
 }
 
-void Follower::eventInCallBack(const geometry_msgs::PointStampedConstPtr& msg){
+void Follower::filteredPersonPositionCallback(const geometry_msgs::PointStampedConstPtr& msg){
+  
+
+  // Save the position as point, in its original frame
   filtered_person_position_ = *msg;
 
+  if(filtered_person_position_.header.frame_id != "base_link")
+    ROS_WARN("Person position not in fixed frame");
+  
+  // Obtain the person's position in the robot's frame
   // TODO wrap exception
-  listener_->transformPoint("base_link", filtered_person_position_, relative_person_position_);
+  listener_->transformPoint("/base_link", filtered_person_position_, relative_person_position_);
+  
+
+  // Update path and trajectory only when the person moves some distance
+
+  double path_distance;
+  double path_minimum_distance_ = 0.25;
+  bool path_empty = true;
+
+  if(complete_person_path_.size() >= 1){
+    geometry_msgs::PointStamped last_path_position = complete_person_path_[complete_person_path_.size()-1];
+    path_empty = false;
+    path_distance = std::sqrt(std::pow(filtered_person_position_.point.x-last_path_position.point.x, 2) + std::pow(filtered_person_position_.point.y-last_path_position.point.y, 2));
+  }
+  
+  if(path_empty || path_distance >= path_minimum_distance_){
+
+    // Update the person's path
+    complete_person_path_.push_back(filtered_person_position_);
+
+    // Update the person's trajectory (that contains poses instead of points)
+    geometry_msgs::PoseStamped filtered_person_pose;
+    
+    filtered_person_pose.header = filtered_person_position_.header;
+    filtered_person_pose.pose.position = filtered_person_position_.point;
+
+    complete_person_trajectory_.header = filtered_person_position_.header;
+    complete_person_trajectory_.poses.push_back(filtered_person_pose);
+
+    // TODO move
+    // Publish complete person path
+    trajectory_publisher_.publish(complete_person_trajectory_);
+
+  }
+  
+
+
+  new_person_position_received_ = true;
 
   ROS_INFO("Absolute person position in frame [%s]: %2.3f, %2.3f",  filtered_person_position_.header.frame_id.c_str(), filtered_person_position_.point.x, filtered_person_position_.point.y);
   ROS_INFO("Relative person position in frame [%s]: %2.3f, %2.3f",  relative_person_position_.header.frame_id.c_str(), relative_person_position_.point.x, relative_person_position_.point.y);
 
-  new_person_position_received_ = true;
-
 }
 
 void Follower::robotPoseCallBack(const geometry_msgs::PoseWithCovarianceStampedConstPtr& msg){
-  start_.header = msg->header;
-  start_.pose = msg->pose.pose;
+  current_robot_position_.header = msg->header;
+  current_robot_position_.pose = msg->pose.pose;
 }
 
 void Follower::updateNavigationGoal(){
@@ -64,6 +116,7 @@ void Follower::updateNavigationGoal(){
   double y = relative_person_position_.point.y;
   double z = relative_person_position_.point.z;
   
+
   // Direct navigation
   if(navigation_type_ == "straight"){
     double d = 0.6;
@@ -77,7 +130,40 @@ void Follower::updateNavigationGoal(){
     goal_.pose.orientation = tf::createQuaternionMsgFromYaw(atan2(final_y,final_x));
     
   } else if(navigation_type_ == "path_following"){
-    // TODO interpolate person trajectory and get point 
+    // TODO interpolate person trajectory and get target  
+
+    bool path_finished = path_target_pointer_ + 1 >= complete_person_trajectory_.poses.size();
+
+    ROS_INFO("path_finished: %s\tpath_target_pointer: %lu/%lu", path_finished?"True":"False", path_target_pointer_, complete_person_trajectory_.poses.size()-1);
+
+    if(!path_finished){
+
+      geometry_msgs::PoseStamped current_target_pose = complete_person_trajectory_.poses[path_target_pointer_];
+      geometry_msgs::PoseStamped next_target_pose = complete_person_trajectory_.poses[path_target_pointer_+1];
+      
+      current_target_pose_publisher_.publish(current_target_pose);
+      next_target_pose_publisher_.publish(next_target_pose);
+
+      double distance_to_current_target = std::sqrt(std::pow(current_robot_position_.pose.position.x-current_target_pose.pose.position.x, 2) + std::pow(current_robot_position_.pose.position.y-current_target_pose.pose.position.y, 2));
+      bool goal_reached = distance_to_current_target < 0.25;
+
+
+      if(goal_reached){
+        // When the current target pose becomes close enough, set the next as goal
+
+        ROS_INFO("Navigation goal reached.");
+
+        path_target_pointer_++;
+
+        goal_.header = filtered_person_position_.header;
+        goal_.pose = next_target_pose.pose;
+        goal_.pose.orientation = tf::createQuaternionMsgFromYaw(atan2(next_target_pose.pose.position.y - current_target_pose.pose.position.y , next_target_pose.pose.position.x - current_target_pose.pose.position.x));
+
+
+      }
+
+    }
+
   } else if(navigation_type_ == "recovery"){
     // TODO look for a person where the other navigation types may have lost the tracking 
   }
@@ -154,7 +240,7 @@ void Follower::updateHeadPosition(){
 
 bool Follower::isthereaPath(geometry_msgs::PoseStamped goal){
   nav_msgs::GetPlan srv;
-  srv.request.start = start_;
+  srv.request.start = current_robot_position_;
   srv.request.goal = goal;
   if (!plan_client_.call(srv)){
     ROS_ERROR("Failed to call service make_plan");
@@ -176,6 +262,12 @@ void Follower::clearCostmaps(){
     ROS_ERROR("Failed to Clear Costmaps");
   }
 }
+
+void Follower::setup(){
+  ROS_INFO("navigation_type: %s", navigation_type_.c_str());
+  ROS_INFO("navigation_stack: %s", navigation_stack_.c_str());
+}
+
 
 void Follower::loop(){
 
@@ -207,21 +299,19 @@ void Follower::loop(){
 int main(int argc, char** argv){
   ros::init(argc, argv, "Follower");
   
-  // create object of the node class
   Follower follower;
-  // setup node frequency
-  double node_frequency = 50;
 
+  double node_frequency = 50;
   ros::Rate loop_rate(node_frequency);
+
+  follower.setup();
 
   while(ros::ok()){
     
     ros::spinOnce();
-
-    // TODO move everything to loop function
     follower.loop();
-    
     loop_rate.sleep();
+
   }
   
   return 0;
